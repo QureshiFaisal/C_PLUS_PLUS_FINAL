3cdf4feb55e245a55c187ba9c9f25d2cf481f5f2
QureshiFaisal <qureshifi@icloud.com>
2022-12-17 19:57:07 +0530

    delegating constructors

--- default_constructor.cpp
+++ default_constructor.cpp
@@ -62,37 +62,37 @@
 
 //eg With three args defined no args constructor
 
-class Player{
-  private:
-  std::string name;
-  int health;
-  int xp;
-public:
+// class Player{
+//   private:
+//   std::string name;
+//   int health;
+//   int xp;
+// public:
 
-void set_name(std::string name_val){
-  name = name_val;
-}
-std::string get_name(){
-  return name;
-}
-Player(){// no args constructor
- name = "None";
-  health = 100;
-  xp = 3;
-}
-Player(std::string name_val, int health_val, int xp_val){// USER DEFINED CONSTRUCTOR THREE ARGS CONSTRUCTOR
-  name = name_val;
-  health = health_val;
-  xp = xp_val;
-}
-};
-int main(){
-Player hero; //Here the no args constructor will be called   
-Player faisal("Faisal", 100, 13);// Since we have  provided a three args constructor, C++ will use the three args constructor while creating this instrance.
-faisal.set_name("Faisal");
-faisal.get_name();
+// void set_name(std::string name_val){
+//   name = name_val;
+// }
+// std::string get_name(){
+//   return name;
+// }
+// Player(){// no args constructor
+//  name = "None";
+//   health = 100;
+//   xp = 3;
+// }
+// Player(std::string name_val, int health_val, int xp_val){// USER DEFINED CONSTRUCTOR THREE ARGS CONSTRUCTOR
+//   name = name_val;
+//   health = health_val;
+//   xp = xp_val;
+// }
+// };
+// int main(){
+// Player hero; //Here the no args constructor will be called   
+// Player faisal("Faisal", 100, 13);// Since we have  provided a three args constructor, C++ will use the three args constructor while creating this instrance.
+// faisal.set_name("Faisal");
+// faisal.get_name();
 
 
 
-  return 0;
-}
+//   return 0;
+// }
--- delegating_constructors.cpp
+++ delegating_constructors.cpp
@@ -18,20 +18,17 @@ public:
     Player(std::string name_val, int health_val, int xp_val);
 };
 
-Player::Player() 
-    : Player {"None",0,0} {
-        cout << "No-args constructor" << endl;
-}
+Player::Player() : Player{ "None", 0, 0 } {
+        std::cout << "No-args constructor" << std::endl;
+    }
 
-Player::Player(std::string name_val) 
-   : Player {name_val,0, 0}  {
-           cout << "One-arg constructor" << endl;
-}
-  
-Player::Player(std::string name_val, int health_val, int xp_val) 
-    : name{name_val}, health{health_val}, xp{xp_val} {
-            cout << "Three-args constructor" << endl;
-}
+    Player::Player(std::string name_val) : Player{ name_val, 0, 0 } {
+        std::cout << "1 arg constructor" << std::endl;
+    }
+
+    Player::Player(std::string name_val, int health_val, int xp_val) : name{ name_val }, health{ health_val }, xp{ xp_val } {
+        std::cout << "3 arg constructor" << std::endl;
+    }
 
 int main() {
     
