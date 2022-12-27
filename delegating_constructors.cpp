81233882ffba94c7846c48f7680d6a924b82f2ef
QureshiFaisal <qureshifi@icloud.com>
2022-12-27 14:44:19 +0530

    deep copy constructor

--- delegating_constructors.cpp
+++ delegating_constructors.cpp
@@ -18,15 +18,18 @@ public:
     Player(std::string name_val, int health_val, int xp_val);
 };
 
-Player::Player() : Player{ "None", 0, 0 } {
+Player::Player() 
+: Player{ "None", 0, 0 } {
         std::cout << "No-args constructor" << std::endl;
     }
 
-    Player::Player(std::string name_val) : Player{ name_val, 0, 0 } {
+Player::Player(std::string name_val) 
+    : Player{ name_val, 0, 0 } {
         std::cout << "1 arg constructor" << std::endl;
     }
 
-    Player::Player(std::string name_val, int health_val, int xp_val) : name{ name_val }, health{ health_val }, xp{ xp_val } {
+Player::Player(std::string name_val, int health_val, int xp_val) 
+    : name{ name_val }, health{ health_val }, xp{ xp_val } {
         std::cout << "3 arg constructor" << std::endl;
     }
 
